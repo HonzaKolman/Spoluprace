d560ac4861805132d936df39bf2a363b0f48ca13
stokr <stokr@PC519.SPSEJECNANET.CZ>
2018-03-20 10:55:06 +0100

    edit Okno

--- SpolecnyProjekt/src/Okno.java
+++ SpolecnyProjekt/src/Okno.java
@@ -3,17 +3,19 @@ import java.awt.FlowLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class Okno extends JFrame{
 	private JLabel label;
 	private JTextField field;
 	private JButton but;
+	private JTextArea area;
 	
 	public Okno() {
 		this.setTitle("XOR šifra");
 		this.setResizable(false);
-		this.setSize(500, 250);
+		this.setSize(600, 350);
 		this.setLayout(new FlowLayout());
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		label = new JLabel("Zadej cestu: ");
@@ -21,6 +23,9 @@ public class Okno extends JFrame{
 		field = new JTextField(40);
 		this.add(field);
 		but = new JButton("Zašifrovat");
+		this.add(but);
+		area = new JTextArea(15, 50);
+		this.add(area);
 		this.setVisible(true);
 	}
 }
